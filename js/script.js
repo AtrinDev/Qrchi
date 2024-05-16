6de719abebea0b90310a080412e164608587f98a
atrin abbasian <atrin.abbasian7@gmail.com>
2024-05-16 22:05:16 +0330

    few changes

--- js/script.js
+++ js/script.js
@@ -1,59 +1,53 @@
-const navbar = document.querySelector('#nav');
-const backwardArrow = document.querySelector('#arrow-backward');
-const forwardArrow = document.querySelector('#arrow-forward');
-
+const navbar = document.querySelector("#nav");
 
 window.addEventListener("scroll", () => {
-    if(window.pageYOffset > 80) {
-        navbar.classList.add("navbar-fixed");
-    }
-    else {
-        navbar.classList.remove("navbar-fixed");
-    }
+  if (window.pageYOffset > 80) {
+    navbar.classList.add("navbar-fixed");
+  } else {
+    navbar.classList.remove("navbar-fixed");
+  }
 });
 
-const multimediSwiper = new Swiper('.swiper', {
-    loop: true,
-    grabCursor: true,
-    spaceBetween: 50,
-    autoplay: {
-        delay: 3000,
-        setTimeout: 3500,
+const multimediSwiper = new Swiper(".swiper", {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 50,
+  autoplay: {
+    delay: 3000,
+    setTimeout: 3500,
+  },
+  breakpoints: {
+    0: {
+      centeredSlides: true,
+      slidesPerView: 1.5,
     },
-    breakpoints: {
-        0: {
-            centeredSlides: true,
-            slidesPerView: 1.5
-        },
-        570: {
-            slidesPerView: 2
-        },
-        768: {
-            slidesPerView: 2.5
-        },
-        992: {
-            slidesPerView: 4
-        },
-        1366: {
-            slidesPerView: 4
-        },
+    570: {
+      slidesPerView: 2,
     },
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+    768: {
+      slidesPerView: 2.5,
     },
+    992: {
+      slidesPerView: 4,
+    },
+    1366: {
+      slidesPerView: 4,
+    },
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
-
 
 $(".collapse-header").click(function () {
-    $(".collapse-item").slideUp();
-    if ($(this).hasClass("active")) {
-       $(this).next().slideUp();
-       $(this).removeClass("active");
-    } else {
-       $(this).next().slideDown();
-       $(".collapse-header").removeClass("active");
-       $(this).addClass("active");
-    }
- });
-
+  $(".collapse-item").slideUp();
+  if ($(this).hasClass("active")) {
+    $(this).next().slideUp();
+    $(this).removeClass("active");
+  } else {
+    $(this).next().slideDown();
+    $(".collapse-header").removeClass("active");
+    $(this).addClass("active");
+  }
+});
