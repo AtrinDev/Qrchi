4c8bab795d3cb8461019229ed073684aa78ee4fb
atrin abbasian <atrin.abbasian7@gmail.com>
2024-05-19 23:15:44 +0330

    hamburger menu

--- js/script.js
+++ js/script.js
@@ -40,6 +40,22 @@ const multimediSwiper = new Swiper(".swiper", {
   },
 });
 
+const initApp = () => {
+  const hamburgerBtn = document.getElementById("hamburger-menu");
+  const mobileMenu = document.getElementById("mobile-menu");
+
+  const toggleMenu = () => {
+    mobileMenu.classList.toggle("hidden");
+    mobileMenu.classList.toggle("flex");
+    hamburgerBtn.classList.toggle("toggle-btn");
+  };
+
+  hamburgerBtn.addEventListener("click", toggleMenu);
+  mobileMenu.addEventListener("click", toggleMenu);
+};
+
+document.addEventListener("DOMContentLoaded", initApp);
+
 $(".collapse-header").click(function () {
   $(".collapse-item").slideUp();
   if ($(this).hasClass("active")) {
--- tailwind.config.js
+++ tailwind.config.js
@@ -41,9 +41,21 @@ module.exports = {
         900: "#1A1A1A",
       },
     },
+    keyframes: {
+      "open-menu": {
+        "0%": { transform: "scaleY(0)" },
+        "80%": { transform: "scaleY(1.2)" },
+        "100%": { transform: "scaleY(1)" },
+      },
+    },
+    animation: {
+      "open-menu": "open-menu 0.5s ease-in-out forwards",
+    },
   },
   plugins: [],
-  safelist: [{
-    pattern: /(primary|secondary|grayScale)/
-}]
+  safelist: [
+    {
+      pattern: /(primary|secondary|grayScale)/,
+    },
+  ],
 };
