4941f0e6d09d5bfaf1f242d2e199250747ed022a
atrin abbasian <atrin.abbasian7@gmail.com>
2024-05-22 21:19:39 +0330

    edit num 4

--- js/script.js
+++ js/script.js
@@ -48,16 +48,20 @@ const multimediSwiper = new Swiper(".swiper", {
 
 const initApp = () => {
   const hamburgerBtn = document.getElementById("hamburger-menu");
+  const closeBtn = document.getElementById("hamburger-menu-close");
   const mobileMenu = document.getElementById("mobile-menu");
+  const mobileNav = document.getElementById("mobile-nav");
 
   const toggleMenu = () => {
     mobileMenu.classList.toggle("hidden");
     mobileMenu.classList.toggle("flex");
-    hamburgerBtn.classList.toggle("toggle-btn");
+    hamburgerBtn.classList.add("toggle-btn");
+    closeBtn.classList.remove("toggle-btn");
+    mobileNav.style.display = "none";
   };
 
   hamburgerBtn.addEventListener("click", toggleMenu);
-  // mobileMenu.addEventListener("click", toggleMenu);
+  closeBtn.addEventListener("click", toggleMenu);
 };
 
 document.addEventListener("DOMContentLoaded", initApp);
--- tailwind.config.js
+++ tailwind.config.js
@@ -41,15 +41,17 @@ module.exports = {
         900: "#1A1A1A",
       },
     },
-    keyframes: {
-      openMenu: {
-        "0%": { transform: "scaleX(0)" },
-        "80%": { transform: "scaleX(1.2)" },
-        "100%": { transform: "scaleX(1)" },
+    extend: {
+      keyframes: {
+        openMenu: {
+          "0%": { transform: "scaleX(0)" },
+          "80%": { transform: "scaleX(1.2)" },
+          "100%": { transform: "scaleX(1)" },
+        },
       },
-    },
-    animation: {
-      "open-menu": "openMenu 0.5s ease-in-out forwards",
+      animation: {
+        "open-menu": "openMenu 0.5s ease-in-out forwards",
+      },
     },
   },
   plugins: [],
